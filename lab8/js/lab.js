283bb501afe36d6bb77aa126700de99b39de3dd2
sean <tmeeks@csumb.edu>
2022-10-04 20:54:25 -0700

    lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -4,3 +4,18 @@
  *
  *           CSUMB
  **/
+
+function ready (){
+  let outputEl = document.querySelector("#output");
+  let oneEl = document.createElement("button");
+  let twoEl = document.createElement("button");
+
+  outputEl.appendChild(oneEl);
+  oneEl.textContent = 'BUTTON';
+  oneEl.style.color= 'red';
+
+  outputEl.appendChild(twoEl);
+  twoEl.textContent = 'BUTTON';
+  twoEl.style.backgroundColor = 'blue';
+  twoEl.style.color='orange';
+}
