79e7035252f92abe3069575c0ea28e8b96b77141
sean <tmeeks@csumb.edu>
2022-10-10 16:20:04 -0700

    lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -21,24 +21,47 @@
 /////update the course name text
 /////update the course description
 /////change the picture
-let showPanel = (e)=> {
-  console.log(e);
 
-  let panel = document.querySelector("#infopanel");
-  console.log(infopanel);
-  panel.style.display = "block";
-
-
-  let p = document.querySelector("#courseName");
-  console.log(courseName);
-  courseName.textContent = "CST 252";
-}
 
 document.addEventListener("DOMContentLoaded", () => {
+  let itemBox = document.querySelector('.itembox');
+  let showPanel = (e)=> {
+    console.log(e);
 
-});
+    let panel = document.querySelector("#infopanel");
+    console.log(infopanel);
+    panel.style.display = "block";
 
+    let p = document.querySelector("#courseName");
+    console.log(courseName);
+    courseName.textContent = "CST 252: Scripting for Multiimedia ";
 
-//1. The exit button
-//2. clicking
-//3. The info panel/popup hides (disappears)
+    let description = document.querySelector('#courseDescription');
+    console.log(description);
+    description.textContent = "A course about learning Javascript.";
+
+    document.querySelector("#courseimage").src="images/cst252.jpg";
+  }
+  itemBox.addEventListener('click',showPanel);
+
+  let theBorder = document.querySelector('.itembox');
+
+  let showBorder = () => {
+    theBorder.style.border = "thick solid";
+}
+
+  let hideBorder = () => {
+    theBorder.style.border = 'none';
+  }
+
+  theBorder.addEventListener('mouseover', showBorder);
+  theBorder.addEventListener('mouseout', hideBorder);
+
+  let exitButton = document.querySelector('#exitbutton');
+  let infoPanel = document.querySelector('#infopanel');
+  let closePanel = () => {
+   infoPanel.style.display = 'none';
+ }
+ exitButton.addEventListener('click',closePanel);
+
+})
