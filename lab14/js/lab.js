6bbc308f632f1bf875b519bde04045af059cdbd3
sean <tmeeks@csumb.edu>
2022-11-08 16:00:11 -0800

    lab 14

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => { // events are live at the same time
 
 
 	//putting the input box into a variable
@@ -10,14 +10,15 @@ document.addEventListener("DOMContentLoaded", () => {
 	////if so
 	/////////create a new div
 	/////////set the div to have a message "you won!"
-	let inputHandler = () => {
-		if (+inputBox.value === '6') {
-			let newBox = document.createElement('div');
-			newBox.classList.add('box');
-			newBox.textContent = "You won!";
+
+	let inputHandler = () => { //function/event that's supposed to occur if input is correct
+		if (+inputBox.value === '6') { //sets input value as a variable
+			let newBox = document.createElement('div'); //creates new div for box
+			newBox.classList.add('box'); //creates a new class in the div
+			newBox.textContent = "You won!"; //adds text to the new box
 		}
 	};
 
-	inputBox.addEventListener('input', inputHandler);
+	inputBox.addEventListener('input', inputHandler); //event occurs when numnber is correctly input into the input box
 
 });
